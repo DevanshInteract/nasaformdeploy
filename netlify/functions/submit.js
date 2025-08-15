86399a60a47293d12fabcb70724e8b7f16d8e2c7
Devansh Katole <87644657+DevanshInteract@users.noreply.github.com>
2025-08-15 22:37:31 +0530

    Update and rename register.js to submit.js

--- netlify/functions/submit.js
+++ netlify/functions/submit.js
@@ -1,33 +1,32 @@
 const { MongoClient } = require("mongodb");
 
+// Get the secret database connection string from Netlify's environment variables
 const mongoUri = process.env.MONGODB_URI;
 
 exports.handler = async function(event) {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
-  }
-
   const client = new MongoClient(mongoUri);
 
   try {
     const data = JSON.parse(event.body);
 
     await client.connect();
-
+    
+    // We'll save this data to a new database and collection for the MUN event
     const collection = client.db("nasaHackathon").collection("munpass");
-
+    
+    // Insert the form data
     await collection.insertOne(data);
 
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "Success! Your registration has been saved." })
     };
-
+    
   } catch (error) {
     console.error("Error:", error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: "Error saving your registration." })
+      body: JSON.stringify({ message: "Error saving your submission." })
     };
   } finally {
     await client.close();
